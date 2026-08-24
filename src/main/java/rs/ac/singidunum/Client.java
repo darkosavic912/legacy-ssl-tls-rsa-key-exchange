93acf42d353ea39fbc199caaaf2f4a4d8053678e
darko_smiley <118219583+darkosavic912@users.noreply.github.com>
2026-08-24 15:09:02 +0200

    style: remove unused imports

--- src/main/java/rs/ac/singidunum/Client.java
+++ src/main/java/rs/ac/singidunum/Client.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.*;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
--- src/main/java/rs/ac/singidunum/Server.java
+++ src/main/java/rs/ac/singidunum/Server.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
